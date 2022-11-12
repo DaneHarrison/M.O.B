abf7d18c756ceb80d4b26efa96e5b1a13cdce5da
Tehillah <tehillahk@outlook.com>
2022-11-11 22:24:34 -0600

    photo read

--- src/presentation/ components/signin.tsx
+++ src/presentation/ components/signin.tsx
@@ -4,15 +4,18 @@ import { useRef, useState } from "react";
 const SignIn = (props: any) => {
   const [image, setImage] = useState();
   const userFace = useRef();
-  const handleSubmit = (event:any) => {
-   
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    alert(`The name you entered was: ${userFace.current.files[0].name}`)
+    alert(`The name you entered was: ${userFace.current.files[0].name}`);
     const formData = new FormData();
-    formData.append("face",userFace.current.files[0])
-    axios.post("/api/")
-  }
-  ;
+    formData.append("face", userFace.current.files[0]);
+    axios
+      .post("/api/authenticate", formData)
+      .then((res) => {
+        alert("File Upload success");
+      })
+      .catch((err) => alert("File Upload Error"));
+  };
 
   return (
     <form className="flex flex-col" onSubmit={handleSubmit}>
