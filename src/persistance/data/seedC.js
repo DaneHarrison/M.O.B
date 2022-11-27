54ed8c21b4ec51e98f979f8dd72d1ba11d0bff37
Dane <info.danewanke@gmail.com>
2022-11-27 15:54:19 -0600

    adding seeding scripts

--- src/persistance/data/seedC.js
+++ src/persistance/data/seedC.js
@@ -1,30 +1,49 @@
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient} from '@prisma/client'
+import fs from 'fs'
+
+import photos from "./fileManifest.json"
 import name from "./randomNames.json"
-const prisma = new PrismaClient()
+import weights from "./weights.json"
+
 async function main() {
-    let currNamePosi = 199;
+    let client = new PrismaClient();
+    let currNamePosi = 22;
+    let index = 199;
+    let submission;
     
     for(let i = 199; i < 4; i++) {
-        const submission = await prisma.user.upsert({
-            create: {
-                Name: name[currNamePosi],
-                Weight: 'Alice', 
-                Photo: ""
-            }
-        })
+        submission = await creatEntry(client, index, name[currNamePosi]);
+        index++;
+
+        if(!submission) {
+            console.log("there was an error seeding the database");
+            return;
+        }
     }
 
-    for(let i = 204; i < 200; i ++) {
+    for(; index < 350 && index < photos.length && submission; index++) {
         if(i - 4 % 8 == 0) {
             currNamePosi++;
         }
 
-        const submission = await prisma.user.upsert({
-            create: {
-                Name: name[currNamePosi],
-                Weight: 'Alice', 
-                Photo: ""
-            }
-        })
+        submission = await creatEntry(client, index, name[currNamePosi]);
+        index++;
+
+        if(!submission) {
+            console.log("there was an error seeding the database");
+            return;
+        }
     }
+}
+
+async function creatEntry(client, index, name) {
+    let results = await client.user.upsert({
+        create: {
+            Name: name,
+            Weight: weights[index], 
+            Photo: fs.readFileSync(photos[index], {flag: 'r'})
+        }
+    });
+
+    return results;
 }
