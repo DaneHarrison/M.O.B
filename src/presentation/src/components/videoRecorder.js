3c9af00aa8ac08c1154d72e86c1763f1289a680f
Dane <info.danewanke@gmail.com>
2023-09-29 14:03:57 -0500

    got video working

--- src/presentation/src/components/videoRecorder.js
+++ src/presentation/src/components/videoRecorder.js
@@ -1,50 +1,64 @@
-import React from 'react'
+import React from 'react';
 
-export default class SelectionView extends React.Component {
+export default class VideoRecorder extends React.Component {
     constructor(props) {
         super(props);
 
-
         this.state = {
-            player: document.getElementById('player'),
-            canvas: document.getElementById('canvas'),
+            player: null,
+            canvas: null,
             constraints: {
-                audio: false,
-                video: true
+                video: true,
             }
-        }
-
-        this.startRecording()
+        };
     }
 
-    startRecording() {
-        navigator.mediaDevices.getUserMedia(this.state.constraints).then((stream) => {
+    componentDidMount() {
+        // Get references to the video and canvas after creation then start recording
+        this.setState({ player: document.getElementById('player')})
+        this.setState({ canvas: document.getElementById('canvas')})
+
+        this.startRecording();
+    }
+
+    async startRecording() {
+        let stream = null
+
+        try {
+            stream = await navigator.mediaDevices.getUserMedia(this.state.constraints);
             this.state.player.srcObject = stream;
-        });
+        } 
+        catch(error) {
+            console.error('[ERROR]: ', error);
+        }
     }
 
     stopRecording() {
-        this.state.player.srcObject.getVideoTracks().forEach(track => track.stop());
+        if(this.state.player && this.state.player.srcObject) {
+            this.state.player.srcObject.getVideoTracks().forEach((track) => track.stop());
+        }
     }
 
     capture() {
-        let jpegDataUri = null  //holds the captured frame after its converted to a data URI
-        let context = this.state.canvas.getContext('2d')
+        let context = null;
+        let jpegDataUri = null;
 
-        context.drawImage(this.state.player, 0, 0, this.state.canvas.width, this.state.canvas.height);
-        jpegDataUri = this.state.canvas.toDataURL('image/jpeg');
+        if(this.state.player && this.state.canvas) {
+            context = this.state.canvas.getContext('2d');
+            context.drawImage(this.state.player, 0, 0, this.state.canvas.width, this.state.canvas.height);
+            jpegDataUri = this.state.canvas.toDataURL('image/jpeg');
 
-        this.stopRecording()
-        this.props.updatePhoto('main', jpegDataUri)
+            this.stopRecording();
+            this.props.updatePhoto('main', jpegDataUri);
+        }
     }
-
 
     render() {
         return (
             <div>
-                <video id="player" controls muted autoplay></video>
+                <video id="player" muted autoPlay></video>
                 <canvas id="canvas" width="300" height="300"></canvas>
             </div>
-        )
+        );
     }
 }
