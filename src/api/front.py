d1970ec45f993dc5dfbe37ace8c0dd9ba7a7d6a8
Zeeshan <zeeshanjaved13@gmail.com>
2023-09-30 14:30:22 -0500

    added functionality for the Img receiving

--- src/api/front.py
+++ src/api/front.py
@@ -4,15 +4,17 @@
 # front is a server responsible for delegating authentication requests to workers
 # once a response is recieved, the response is then sent back to the user
 # --------------------------------
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, Response
 from flask_restful import Resource, Api
-import threading, json, requests
+import threading, json, requests, os, urllib.request
+from werkzeug.utils import secure_filename
 
 HOST = 'localhost'              # The front servers address
 PORT = 5000                     # The front servers port
 URL = 'http://localhost:4000/'  # URL where the worker(s) can be reached   
 
-app = Flask(__name__)   # Flask server instance
+app = Flask(__name__, template_folder='../presentation/src/templates/')   # Flask server instance
+app.config['UPLOAD_FOLDER'] = './'
 api = Api(app)          # Controls the Flask server API 
     
 class ProcessImg(Resource):
@@ -25,6 +27,26 @@ class ProcessImg(Resource):
         res = res.json()
 
         return res # Forward the response back to the user
+
+class Index(Resource):
+    def get(self,):
+        return Response(render_template('index.html'), mimetype='text/html')
     
-api.add_resource(ProcessImg, '/')
+    def post(self,):
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file, kinda cringe'})
+        
+        file = request.files['file']
+        if file.filename == "":
+            return jsonify({'error':'No image selected, kinda weird'})
+        
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        return Response("Success", mimetype='text/html')
+    
+
+#api.add_resource(ProcessImg, '/')
+api.add_resource(Index,'/')
 app.run(host=HOST, port=PORT, debug=True)
